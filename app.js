40ed7512093132f3e95c32597cb9f9e5c82b30d2
Renan de Oliveira Silva <renan-oliveira@altamogiana.com.br>
2024-10-22 16:36:42 -0300

    Desenv

--- app.js
+++ app.js
@@ -18,6 +18,22 @@ app.set('view engine', 'ejs');
 // Rotas
 app.use("/", mainRouter);
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+  
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+
 // Rodando o servidor na porta 3000
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT} !!!`);
