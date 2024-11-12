c258a194d3f3ea78d536f93fc51b84f09ee37dad
RenanOliveiraSilva <renandeoliveirasilva123@gmail.com>
2024-11-12 07:45:02 -0300

    modified:   routes/router.js

--- routes/router.js
+++ routes/router.js
@@ -8,23 +8,12 @@ router.get('/', function(req, res, next) {
 });
 
 // Renderizando a página login
-<<<<<<< HEAD
-router.get('/login', function(req, res, next) {
-  res.render('home/cadastrar_user');
-});
-
-router.get('/home', usuarioController.listarUsuarios);
-
-//Cadastrar usuário
-router.post('/cad_user', usuarioController.inserirUsuarios);
-=======
 router
     .get('/login', function(req, res, next) {
       res.render('landing/login');
     })
     .post('/login', usuarioController.inserirUsuarios);
 
->>>>>>> a79070fd6ccf54303952e38d60c3a7efffcb9e55
 
 //Validação do usuário
 router.post('/login', usuarioController.logarUsuario);
